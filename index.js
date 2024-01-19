8e19c24b6d0e72a53e014ac7d926d4c5b3b079d6
herbert <408284566@qq.com>
2024-01-19 19:06:21 +0800

    feat: 增加时区参数

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-// const path = require("path");
 const fs = require("fs");
 const execSync = require("child_process").execSync; //同步子进程
 
 const pluginName = "get-version-plugin";
 
+/**
+ * @typedef {Object} GetVersionPluginOptions
+ * @property {string} [path] - 路径
+ * @property {number} [offset] - 偏移时间（小时）
+ */
+
 class GetVersionPlugin {
+  /**
+   * @param {GetVersionPluginOptions} options - 插件选项
+   */
   constructor(options) {
     if (options.path) this.path = options.path;
+    if (options.offset) this.offset = options.offset;
   }
 
   apply(compiler) {
@@ -16,6 +25,7 @@ class GetVersionPlugin {
 
     const name = execSync("git show -s --format=%cn").toString().trim(); //姓名
     let date = new Date(execSync("git show -s --format=%cd").toLocaleString()); //日期 这个是时区0 的， 在前端输出时候要再 new Date(date).toLocaleString()
+    date.setHours(date.getHours() + (this.offset || 0)).toLocaleString()  
     let message = execSync("git show -s --format=%s").toString().trim(); //说明
     let commit = execSync("git show -s --format=%h").toString().trim();
     let branch = execSync("git rev-parse --abbrev-ref HEAD").toString().trim(); // 分支
